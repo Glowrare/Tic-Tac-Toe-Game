fbcd773ba4b0f74e86cd039c483390a4a0726a87
Glowrare <aajibolanle@yahoo.com>
2021-06-02 13:04:34 +0100

    Feauture added: Show noughts and crosses on click

--- src/components/layout/TheBoard.vue
+++ src/components/layout/TheBoard.vue
@@ -1,18 +1,129 @@
 <template>
   <base-board-bg>
-    <div class="board" id="board">
-      <div class="cell board-cell"></div>
-      <div class="cell board-cell"></div>
-      <div class="cell board-cell"></div>
-      <div class="cell board-cell"></div>
-      <div class="cell board-cell"></div>
-      <div class="cell board-cell"></div>
-      <div class="cell board-cell"></div>
-      <div class="cell board-cell"></div>
-      <div class="cell board-cell"></div>
+    <div
+      class="board"
+      id="board"
+      :class="[crossClass ? 'cross' : 'nought']"
+      @click="isCross"
+    >
+      <div
+        class="cell board-cell"
+        :class="[boardCell.one ? playClass.one : '']"
+        @click.once="addPlayClass('cellOne')"
+        @click="$emit('cell-click', 'one')"
+      ></div>
+      <div
+        class="cell board-cell"
+        :class="[boardCell.two ? playClass.two : '']"
+        @click.once="addPlayClass('cellTwo')"
+        @click="$emit('cell-click', 'two')"
+      ></div>
+      <div
+        class="cell board-cell"
+        :class="[boardCell.three ? playClass.three : '']"
+        @click.once="addPlayClass('cellThree')"
+        @click="$emit('cell-click', 'three')"
+      ></div>
+      <div
+        class="cell board-cell"
+        :class="[boardCell.four ? playClass.four : '']"
+        @click.once="addPlayClass('cellFour')"
+        @click="$emit('cell-click', 'four')"
+      ></div>
+      <div
+        class="cell board-cell"
+        :class="[boardCell.five ? playClass.five : '']"
+        @click.once="addPlayClass('cellFive')"
+        @click="$emit('cell-click', 'five')"
+      ></div>
+      <div
+        class="cell board-cell"
+        :class="[boardCell.six ? playClass.six : '']"
+        @click.once="addPlayClass('cellSix')"
+        @click="$emit('cell-click', 'six')"
+      ></div>
+      <div
+        class="cell board-cell"
+        :class="[boardCell.seven ? playClass.seven : '']"
+        @click.once="addPlayClass('cellSeven')"
+        @click="$emit('cell-click', 'seven')"
+      ></div>
+      <div
+        class="cell board-cell"
+        :class="[boardCell.eight ? playClass.eight : '']"
+        @click.once="addPlayClass('cellEight')"
+        @click="$emit('cell-click', 'eight')"
+      ></div>
+      <div
+        class="cell board-cell"
+        :class="[boardCell.nine ? playClass.nine : '']"
+        @click.once="addPlayClass('cellNine')"
+        @click="$emit('cell-click', 'nine')"
+      ></div>
     </div>
   </base-board-bg>
 </template>
+
+<script>
+export default {
+  emits: ["cell-click"],
+  data() {
+    return {
+      crossClass: true,
+      boardCell: {
+        one: false,
+        two: false,
+        three: false,
+        four: false,
+        five: false,
+        six: false,
+        seven: false,
+        eight: false,
+        nine: false,
+      },
+      playClass: {
+        one: "",
+        two: "",
+        three: "",
+        four: "",
+        five: "",
+        six: "",
+        seven: "",
+        eight: "",
+        nine: "",
+      },
+      number: "",
+    };
+  },
+  computed: {
+    setPlayClass() {
+      if (this.crossClass) {
+        return "cross";
+      } else {
+        return "nought";
+      }
+    },
+    getNumber() {
+      return this.number.substring(4).toLowerCase();
+    },
+  },
+  methods: {
+    isCross() {
+      this.crossClass = !this.crossClass;
+    },
+    playClassSetter(cell) {
+      this.number = cell;
+      if (cell) {
+        this.boardCell[this.getNumber] = true;
+        this.playClass[this.getNumber] = this.setPlayClass;
+      }
+    },
+    addPlayClass(cellNo) {
+      this.playClassSetter(cellNo);
+    },
+  },
+};
+</script>
 
 <style scoped>
 .board {
--- src/pages/GamePlay.vue
+++ src/pages/GamePlay.vue
@@ -1,6 +1,6 @@
 <template>
   <base-button>Game Mode</base-button>
-  <the-board></the-board>
+  <the-board @cell-click="cellClick"></the-board>
   <result-message class="result-message" v-show="gameEnd"></result-message>
   <quit-message class="quit-message" v-show="quitRequest"></quit-message>
 </template>
@@ -21,6 +21,21 @@ export default {
       gameEnd: false,
       quitRequest: false,
     };
+  },
+  methods: {
+    cellClick(cell) {
+      if (cell === "one") {
+        console.log("one time");
+      } else if (cell === "two") {
+        console.log("two time");
+      } else if (cell === "three") {
+        console.log("three time");
+      } else if (cell === "four") {
+        console.log("four time");
+      } else {
+        console.log("some other");
+      }
+    },
   },
 };
 </script>
