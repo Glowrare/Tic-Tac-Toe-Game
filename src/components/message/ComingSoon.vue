f847ba8e51fb2045abb690d4b4ebcc14d7adada8
Glowrare <aajibolanle@yahoo.com>
2021-06-10 13:07:50 +0100

    Coming soon component added for computer play

---
+++ src/components/message/ComingSoon.vue
@@ -0,0 +1,15 @@
+<template>
+  <base-modal>
+    <template v-slot:gameMessage> Coming Soon! </template>
+    <template v-slot:otherModalContent>
+      <base-flex-wrapper justifyContent="center" class="ft60"
+        >😁
+      </base-flex-wrapper>
+    </template>
+  </base-modal>
+</template>
+<style scoped>
+.ft60 {
+  font-size: 60px;
+}
+</style>
--- src/pages/GameMode.vue
+++ src/pages/GameMode.vue
@@ -1,26 +1,36 @@
 <template>
   <base-button>Game Mode</base-button>
   <base-board-bg>
-    <base-button :hoverable="true" id="versusComp"
-      ><router-link
-        class="block-display"
-        :to="`/game-play/${theme}/versus-computer`"
-        >vs. Computer
-      </router-link></base-button
+    <base-button :hoverable="true" id="versusComp" @click="comingSoonMessage"
+      >vs. Computer</base-button
     >
-    <base-button :hoverable="true" id="multiplayer"
-      ><router-link
-        class="block-display"
-        :to="`/game-play/${theme}/multiplayer`"
+    <base-button :hoverable="true" id="multiplayer">
+      <router-link class="block-display" :to="`/game-play/${theme}/multiplayer`"
         >Multiplayer</router-link
-      ></base-button
-    >
+      >
+    </base-button>
   </base-board-bg>
+  <coming-soon v-show="comingSoon"></coming-soon>
 </template>
 
 <script>
+import ComingSoon from "../components/message/ComingSoon";
 export default {
+  data() {
+    return {
+      comingSoon: false,
+    };
+  },
+  components: { ComingSoon },
   props: ["theme"],
+  methods: {
+    comingSoonMessage() {
+      this.comingSoon = true;
+      setTimeout(() => {
+        this.comingSoon = false;
+      }, 1500);
+    },
+  },
 };
 </script>
 
